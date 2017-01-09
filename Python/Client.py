b8d55fb5b41f3c64a1bce84e6f763e0ec3b3e1e7
Lucas-Doucette <lucasdoucette5@gmail.com>
2017-01-09 11:33:44 -0400

    Update Client.py

--- Python/Client.py
+++ Python/Client.py
@@ -1,7 +1,7 @@
 import socket               
 
 s = socket.socket()        
-host = '192.168.0.191' #ip of Server (PI)?
+host = '192.168.0.10' #ip of Server (PI)?
 port = 12345               
 s.connect((host, port))
 print(s.recv(1024))
