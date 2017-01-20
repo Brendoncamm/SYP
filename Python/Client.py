1d3ee70dc600531475864f1b56a76b52cc012008
Lucas-Doucette <lucasdoucette5@gmail.com>
2017-01-20 17:22:15 -0400

    Delete Client.py

--- Python/Client.py
+++
@@ -1,8 +0,0 @@
-import socket               
-
-s = socket.socket()        
-host = '192.168.0.10' #ip of Server (PI)?
-port = 12345               
-s.connect((host, port))
-print(s.recv(1024))
-s.close()
