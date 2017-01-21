4d42a4d3972bbf6d14ce65b9a2cdeb0ee9d4354e
Dylan Humber <tLJack@users.noreply.github.com>
2017-01-21 19:06:55 -0400

    Update Server.py
    Script ready for test with Arduino

--- Python/Server.py
+++ Python/Server.py
@@ -1,7 +1,10 @@
 import socket
 import os
 import binascii
+import arduino
 from subprocess import check_output
+
+controller = Arduino_Controller(0x08)
 
 s = socket.socket()
 #host = '192.168.0.10'# IP of Server (PI?)
@@ -10,10 +13,12 @@ port = 12345
 s.bind((host, port))
 
 s.listen(5)
+c, addr = s.accept()
 while True:
-  print('waiting for receive')
-  c, addr = s.accept()
-  data=bytes(c.recv(1024))
+  #print('waiting for receive')
+  #c, addr = s.accept()
+  data=bytes(c.recv(16))
+  controller.write(data)
   print(len(data))
   #os.system('clear')
   #c.close()
