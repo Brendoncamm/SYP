e4d2b2b5318168b155ccfcd7edb19d7be7136afc
Dylan Humber <tLJack@users.noreply.github.com>
2017-01-21 15:04:35 -0400

    Server script now obtains it's local IP address automatically.

--- Python/Server.py
+++ Python/Server.py
@@ -1,16 +1,19 @@
 import socket
 import os
+import binascii
+from subprocess import check_output
 
 s = socket.socket()
-host = '192.168.0.10'# IP of Server (PI?)
+#host = '192.168.0.10'# IP of Server (PI?)
+host = check_output(['hostname', '-I']).strip() #Strip to remove newline char
 port = 12345
 s.bind((host, port))
 
 s.listen(5)
 while True:
+  print('waiting for receive')
   c, addr = s.accept()
-  data=c.recv(1024)
-  print(data)
-  os.system('clear')
-  c.close()
-
+  data=bytes(c.recv(1024))
+  print(len(data))
+  #os.system('clear')
+  #c.close()
