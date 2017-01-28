d535dba20d66e162152259f1ff8aa8994bbc50f6
Lucas-Doucette <lucasdoucette5@gmail.com>
2017-01-28 18:48:00 -0400

    Update arduino.py

--- Python/arduino.py
+++ Python/arduino.py
@@ -13,17 +13,15 @@ import smbus
 ##bus.write_i2c_block_data(DEVICE_ADDRESS, 0x01, test_block)
 
 class Arduino_Controller(object):
-    """
-    Provides a wrapper for communicating with the Arduino over I2C/SMBUS.
-    """
+    #  Provides a wrapper for communicating with the Arduino over I2C/SMBUS.
     def __init__(self, address):
         self.address = address
         self.bus = smbus.SMBus(1)
 
     def write_axes(self, axes):
-    """Breaks received 16 byte string into list of 4 byte floats.
-    Then writes them to the bus.
-    """
+    #Breaks received 16 byte string into list of 4 byte floats.
+    #Then writes them to the bus.
+    
     #I2C block data write requires a list, so we will build it from the byte array.
     axis_list = list(axes)
     self.bus.write_i2c_block_data(self.address, 0x01, axis_list)
