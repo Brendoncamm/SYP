e38b247487a5e6141faba20fe883f67946ea36da
Lucas-Doucette <lucasdoucette5@gmail.com>
2017-02-21 17:11:36 -0400

    Create Drone.h

---
+++ Arduino/Drone.h
@@ -0,0 +1,32 @@
+/*
+  Drone.h - Library for Drone Controller.
+  Created by Lucas Doucette, February 17, 2017.
+*/
+#ifndef Drone_h
+#define Drone_h
+
+#include "Arduino.h"
+#include <Wire.h>
+#include<Servo.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_BMP085_U.h>
+#include <Adafruit_L3GD20_U.h>
+#include <Adafruit_10DOF.h>
+
+class Drone
+{
+  public:
+    Drone();
+    float get_sensorRoll();
+    float get_sensorPitch();
+    float get_sensorYaw();
+    float get_sensorAltitude();
+    float PID_Calculate(float Setpoint, float SenseRead, float kp, float kd, float ki );
+    void initSensors();
+    void initESCs(int MotorPin1, int MotorPin2,int MotorPin3,int MotorPin4);
+    
+ 
+};
+
+#endif
