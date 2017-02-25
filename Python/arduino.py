5393b64b818f93cd92857cde0a4e7c58b71864e2
Dylan Humber <tLJack@users.noreply.github.com>
2017-02-25 14:31:17 -0400

    Updated class definition to use serial communications bus.

--- Python/arduino.py
+++ Python/arduino.py
@@ -1,36 +1,17 @@
-#Test script for writing too I2C bus.
-#For test purposes, assuming target address is 0x08
+# Test script for writing too serial bus.
+# For test purposes, assuming target address is 0x08
 # 2017-01-13 Auth: Dylan
 
-import smbus
-import subprocess
-
-##bus = smbus.SMBus(1)
-##
-##DEVICE_ADDRESS = 0x08
-##
-##test_block = [0x11, 0x22]
-##
-##bus.write_i2c_block_data(DEVICE_ADDRESS, 0x01, test_block)
+import serial
 
 class Arduino_Controller(object):
     #  Provides a wrapper for communicating with the Arduino over I2C/SMBUS.
-    def __init__(self, address):
-        self.address = address
-        self.bus = smbus.SMBus(1)
+    def __init__(self, baud):
+        self.baudrate = baud
+        self.serial_bus = serial.Serial('/dev/ttyACM0', baud)
 
     def write_axes(self, axes):
-    #Breaks received 16 byte string into list of 4 byte floats.
-    #Then writes them to the bus.
-    
-    #I2C block data write requires a list, so we will build it from the byte array.
-        axis_list = list(axes)
-        
-        try:
-            self.bus.write_i2c_block_data(self.address, 0x01, axis_list)
-        except:
-            subprocess.call(['i2cdetect','-y','1'])
-        
+        self.serial_bus.write(axes)
 
     def write_button(self, button):
         pass
