9ebff0a65c4a2531b8c6e991c2d38070799725bf
Dylan Humber <tLJack@users.noreply.github.com>
2017-02-28 13:03:39 -0400

    Tested read and send float functions

--- Arduino/Serial_Comms_test.ino
+++ Arduino/Serial_Comms_test.ino
@@ -2,8 +2,30 @@ void setup() {
   Serial.begin(9600);
 }
 
+//from stackoverflow Nils Pipenbrinck
+void send_float (float arg) {
+  byte * data = (byte *) &arg;
+  Serial.write(data, sizeof(arg));
+}
+
+float read_float () {
+ union{
+   float a;
+   unsigned char bytes[4];
+ } data;
+ for (int i=0; i<4; i++) {
+   data.bytes[i] = Serial.read();
+ } 
+ float test = data.a;
+ return(test);
+}
+
+float recv;
 void loop() {
-  if (Serial.available()) {
-    Serial.write(Serial.read());
+  if (Serial.available() == 4) {
+    //Serial.read();
+    recv = read_float();
+    send_float(recv);
+    //Serial.print(sizeof(recv));
   }
 }
